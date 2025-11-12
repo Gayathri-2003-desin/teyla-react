d5a00d787da60721a75ade3e2c7aeeaf79181e8e
gayathri-2003-design <gayathrikh11@gmail.com>
2025-11-12 14:50:38 +0530

    change file name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,10 @@ import Navbar from "../components/Navbar.jsx";
 import VideoBackground from "../components/Home/VideoBackground.jsx";
 import Where from "./Where.jsx";
 import Experience from "./Experience.jsx";
-import About from "../components/Home/about.jsx";
+import About from "../components/Home/About.jsx";
 import ImageSlider from "../components/Home/ImageSlider.jsx";
-import Kerala from "../components/Home/kerala.jsx"; 
-import Ayurveda from "../components/Home/ayurveda.jsx";
+import Kerala from "../components/Home/Kerala.jsx"; 
+import Ayurveda from "../components/Home/Ayurveda.jsx";
 import Popular from "../components/Home/Popular.jsx";
 import Footer from "../components/Footer.jsx";
 
