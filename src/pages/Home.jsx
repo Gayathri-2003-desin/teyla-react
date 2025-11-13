8e2ee29472b50aafd4e61268e8b972d0ceb665d9
gayathri-2003-design <gayathrikh11@gmail.com>
2025-11-13 15:56:38 +0530

    Add Kerala.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import Where from "./Where.jsx";
 import Experience from "./Experience.jsx";
 import About from "../components/Home/Abouts.jsx";
 import ImageSlider from "../components/Home/ImageSlider.jsx";
-import State from "../components/Home/State.jsx"
+import Kerala from "../components/Home/Kerala.jsx";
 import Ayurveda from "../components/Home/Ayurveda.jsx";
 import Popular from "../components/Home/Popular.jsx";
 import Footer from "../components/Footer.jsx";
