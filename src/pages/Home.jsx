871010d5c81c97d489ab1abd73615cbf9cacdbca
gayathri-2003-design <gayathrikh11@gmail.com>
2025-11-13 15:40:58 +0530

    change kerala to state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import Where from "./Where.jsx";
 import Experience from "./Experience.jsx";
 import About from "../components/Home/Abouts.jsx";
 import ImageSlider from "../components/Home/ImageSlider.jsx";
-import Kerala from "../components/Home/Kerala.jsx";
+import State from "../components/Home/State.jsx"
 import Ayurveda from "../components/Home/Ayurveda.jsx";
 import Popular from "../components/Home/Popular.jsx";
 import Footer from "../components/Footer.jsx";
